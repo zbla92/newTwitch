94ff0ddb7c5d1d01997bc4f1c357fcf07261a60a
Zbla92 <milanblaz2580456@gmail.com>
2019-07-29 21:49:18 +0200

    initialization in progress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+const PageOne = () => (
+        <div>
+            PaageOne
+            <Link to='/pagetwo'>Navigate to Page two</Link>
+        </div>
+    )
+
+const PageTwo = () => (
+        <div>
+            PageTwo
+            <button>click me</button>
+        </div>
+    )
+const PageThree = () => <div>PageThree</div>
+
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+           <BrowserRouter>
+           <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" component={PageTwo} />
+                <Route path="/pagetwo/pagethree" component={PageThree} />
+           </div>
+           </BrowserRouter> 
+        </div>
+    )
 }
 
 export default App;
